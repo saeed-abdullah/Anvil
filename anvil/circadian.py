338f4e47709ee59190024a927e3cbfa6aacce675
Saeed Abdullah <sma249@cornell.edu>
2016-05-23 17:33:17 -0400

    Restructures circadin module

--- anvil/circadian.py
+++ anvil/circadian.py
@@ -10,366 +10,361 @@
 """
 
 
-import datetime
-import numpy as np
+import datetime as dt
 import pandas as pd
-import scipy
 
-class CircadianAnalysis(object):
+
+"""
+Utility functions for circadian analysis.
+"""
+
+
+def inter_daily_stability(df, value_col,
+                          hour_col='hour'):
     """
-    Utility functions for circadian analysis.
+    Calculates interdaily stability (IS) from hourly data.
+
+    Higher IS means better rhythmicity. The implementation
+    follows the formula (1) from Witting et al. (pg. 565).
+
+
+    Witting, W., et al. "Alterations in the circadian rest-activity rhythm
+    in aging and Alzheimer's disease."
+    Biological psychiatry 27.6 (1990): 563-572.
+
+    Parameters
+    ----------
+    df : DataFrame.
+    value_col : str
+        Column to calculate daily stability.
+    hour_col : str
+        Column indicating hourly values.
+
+    Returns
+    -------
+    float
+        Value indicating inter daily stability.
+
     """
 
-    @staticmethod
-    def inter_daily_stability(df, value_col,
-                              hour_col='hour'):
-        """
-        Calculates interdaily stability (IS) from hourly data.
+    hour_count = 24
+    mean = df[value_col].mean()
+    N = len(df)
 
-        Higher IS means better rhythmicity. The implementation
-        follows the formula (1) from Witting et al. (pg. 565).
+    denom_sum_val = sum((df[value_col] - mean)**2)
+    denominator = hour_count * denom_sum_val
 
+    l = []
+    for k, v in df.groupby(hour_col):
+        l.append((v[value_col].mean() - mean)**2)
 
-        Witting, W., et al. "Alterations in the circadian rest-activity rhythm
-        in aging and Alzheimer's disease."
-        Biological psychiatry 27.6 (1990): 563-572.
+    nom = sum(l) * N
 
-        Parameters
-        ----------
-        df: DataFrame.
-        value_col: Column to calculate daily stability.
-        hour_col: Column indicating hourly values.
-        """
-
-        hour_count = 24
-        mean = df[value_col].mean()
-        N = len(df)
-
-        denom_sum_val = sum((df[value_col] - mean)**2)
-        denominator = hour_count * denom_sum_val
-
-        l = []
-        for k, v in df.groupby(hour_col):
-            l.append((v[value_col].mean() - mean)**2)
-
-        nom = sum(l) * N
-
-        return nom/denominator
-
-    @staticmethod
-    def inter_daily_stability_across_users(df, value_col,
-                                           hour_col='hour',
-                                           user_col='user_id'):
-        """
-        Utility function for calculating IS across users.
-        """
-
-        l = []
-        for k, v in df.groupby(user_col):
-            stability = CircadianAnalysis.inter_daily_stability(v,
-                                                                value_col=value_col,
-                                                                hour_col=hour_col)
-            l.append({'stability': stability, user_col: k})
-
-        return pd.DataFrame(l)
-
-    @staticmethod
-    def intra_daily_variability(df, value_col):
-        """
-        Calculate intra-daily variability (IV).
-
-        High IV indicates fragmented day. The implementation
-        follows the formula (1) from Witting et al. (pg. 565).
+    return nom/denominator
 
 
-        Parameters
-        ----------
-        df: DataFrame. It must be sorted by date (ascending).
-        value_col: Column to compute IV.
-        """
+def intra_daily_variability(df, value_col):
+    """
+    Calculate intra-daily variability (IV).
 
-        s = (df[value_col] - df[value_col].shift(1))**2
-        nom = len(df) * sum(s.fillna(0).values)
-
-        mean = df[value_col].mean()
-        N = len(df)
-
-        denom = (N - 1) * sum((df[value_col] - mean)**2)
-
-        return nom/denom
-
-    @staticmethod
-    def intra_daily_variability_across_users(df, value_col,
-                                             user_col='user_id'):
-        """
-        Utility function for calculating IV across users
-        """
-
-        l = []
-        for k, v in df.groupby(user_col):
-            variability = CircadianAnalysis.intra_daily_variability(v, value_col)
-            l.append({'variability': variability, user_col: k})
-
-        return pd.DataFrame(l)
-
-    @staticmethod
-    def sort_by_hourly_values(df, value_col,
-                              hour_col='hour'):
-        """
-        Sorts by average hourly values.
-
-        Performs sorting by averaging values across
-        hours. The value returns here can be used
-        for M10 (most active 10 hours) and L5 (least
-        active 5 hours) as defined in Witting et al.
-
-        Parameters
-        ----------
-
-        df: DataFrame.
-        value_col: Column to compute average values.
-        hour_col: Column denoting hours.
+    High IV indicates fragmented day. The implementation
+    follows the formula (1) from Witting et al. (pg. 565).
 
 
-        Returns
-        -------
+    Parameters
+    ----------
+    df : DataFrame.
+        It must be sorted by date (ascending).
+    value_col : str
+        Column to compute daily variability.
 
+    Returns
+    -------
+    float
+        Computed variability score.
+
+    """
+
+    s = (df[value_col] - df[value_col].shift(1))**2
+    nom = len(df) * sum(s.fillna(0).values)
+
+    mean = df[value_col].mean()
+    N = len(df)
+
+    denom = (N - 1) * sum((df[value_col] - mean)**2)
+
+    return nom/denom
+
+
+def sort_by_hourly_values(df, value_col,
+                          hour_col='hour'):
+    """
+    Sorts by average hourly values.
+
+    Performs sorting by averaging values across
+    hours. The value returns here can be used
+    for M10 (most active 10 hours) and L5 (least
+    active 5 hours) as defined in Witting et al.
+
+    Parameters
+    ----------
+
+    df : DataFrame
+    value_col : str
+        Column to compute average values.
+    hour_col : str
+        Column denoting hours. Default is 'hour'.
+
+
+    Returns
+    -------
+    list
         A sorted list (ascending) of tuples (h, v)
         with first element is the hour and second
         element is average value.
-        """
+    """
 
-        d = {}
-        for k, v in df.groupby(hour_col):
-            d[k] = v[value_col].mean()
+    d = {}
+    for k, v in df.groupby(hour_col):
+        d[k] = v[value_col].mean()
 
-        # sort by value
-        return sorted(d.items(), key=lambda z: z[1])
+    # sort by value
+    return sorted(d.items(), key=lambda z: z[1])
 
-    @staticmethod
-    def _convert_timestamp_to_decimal(timeseries,
-                                      should_convert=False):
-        """
-        Convert timestamp to decimal value for SRM.
 
-        Conversion happens using hour + minute/60 formula,
-        so, 08:45 would be 8.75.
+def _convert_timestamp_to_decimal(timeseries,
+                                  should_convert=False):
+    """
+    Convert timestamp to decimal value for SRM.
 
-        Parameters
-        ----------
+    Conversion happens using hour + minute/60 formula,
+    so, 08:45 would be 8.75.
 
-        timeseries: A series with timestamp or datetimes.
-        should_convert: If the timeseries should be converted
-                        using `pd.to_datetime` function.
-        """
+    Parameters
+    ----------
 
-        if should_convert:
-            timeseries = pd.to_datetime(timeseries)
-        return timeseries.map(lambda z: z.hour + z.minute/60)
+    timeseries : Series
+        A series with timestamp or datetimes.
+    should_convert : bool
+        If the timeseries should be converted
+        using `pd.to_datetime` function.
 
-    @staticmethod
-    def _purge_srm_outliers(series,
-                            lower_limit,
-                            upper_limit):
-        """
-        Removes outliers before calculating SRM.
+    Returns
+    -------
+    Series
+        Series containing converted values in decimal.
+    """
 
-        Outliers are points beyond given lower and
-        upper limit, i.e., p < lower_limit or
-        p > upper_limit.
+    if should_convert:
+        timeseries = pd.to_datetime(timeseries)
+    return timeseries.map(lambda z: z.hour + z.minute/60)
 
-        Parameters
-        ----------
 
-        series: A series containing SRM scores.
-        lower_limit: Lower limit.
-        upper_limit: Upper limit.
+def _purge_srm_outliers(series,
+                        lower_limit,
+                        upper_limit):
+    """
+    Removes outliers before calculating SRM.
 
-        Returns
-        -------
+    Outliers are points beyond given lower and
+    upper limit, i.e., p < lower_limit or
+    p > upper_limit.
 
-        A new DataFrame with valid points.
-        """
+    Parameters
+    ----------
 
-        filtering_f = lambda z: z >= lower_limit and z <= upper_limit
-        return series[series.map(filtering_f)]
+    series: Series
+        SRM scores.
+    lower_limit : float
+        Lower limit.
+    upper_limit : float
+        Upper limit.
 
-    @staticmethod
-    def _srm_preprocssing(series):
-        """
-        Pre-processing for SRM calculation.
+    Returns
+    -------
+    Series
+        A new series with valid points only.
+    """
 
-        This function does the followings:
-        1. Converts timestamp to decimal values
-        2. Removes outliers with > 1.5 * SD.
+    filtering_f = lambda z: z >= lower_limit and z <= upper_limit
+    return series[series.map(filtering_f)]
 
-        Parameters
-        ----------
-        series: Series containing values
 
-        Returns
-        -------
-        A new Series.
-        """
+def _srm_preprocssing(series):
+    """
+    Pre-processing for SRM calculation.
 
-        series = CircadianAnalysis._convert_timestamp_to_decimal(series)
-        mean = series.mean()
-        std = series.std()
+    This function does the followings:
+    1. Converts timestamp to decimal values
+    2. Removes outliers with > 1.5 * SD.
 
-        if std < 0.5:
-            return series
+    Parameters
+    ----------
+    series: Series
+        Timestamps of a given SRM event.
 
-        lower_limit = mean - 1.5 * std
-        upper_limit = mean + 1.5 * std
-        series = CircadianAnalysis._purge_srm_outliers(series,
-                                                       lower_limit=lower_limit,
-                                                       upper_limit=upper_limit)
+    Returns
+    -------
+    Series
+        A new series with timestamps converted to
+        decimals and outliers purged.
+    """
 
+    series = _convert_timestamp_to_decimal(series)
+    mean = series.mean()
+    std = series.std()
+
+    if std < 0.5:
         return series
 
-    @staticmethod
-    def _calculate_srm_hit(series,
-                           lower_limit,
-                           upper_limit):
-        """
-        Calculates number of hits.
+    lower_limit = mean - 1.5 * std
+    upper_limit = mean + 1.5 * std
+    series = _purge_srm_outliers(series, lower_limit=lower_limit,
+                                 upper_limit=upper_limit)
 
-        It is the number of values falling within
-        the given range.
-        """
-
-        filtering_f = lambda z: z >= lower_limit and z <= upper_limit
-
-        return sum(series.map(filtering_f))
-
-    @staticmethod
-    def calculate_srm(df, target_col,
-                      time_col='completion_time',
-                      min_samples=3):
-        """
-        Calculates SRM score.
-
-        Parameters
-        ----------
-
-        df: DataFrame.
-        target_col: Column with target names. Individual
-                    hits would be calculated for each
-                    targets.
-        time_col: Column containing timestamps.
-        min_samples: Minimum samples for calculating hit
-                     for a given column. Default is 3 (40%
-                     of a week).
-        """
-
-        hit_range = 45/60  # "hit" if falls within 45 minute
-
-        l = []
-
-        for k, v in df.groupby(target_col):
-            series = v.loc[:, time_col]
-            series = CircadianAnalysis._srm_preprocssing(series)
-            if len(series) >= min_samples:
-
-                mean = series.mean()
-
-                lower = mean - hit_range
-                upper = mean + hit_range
-
-                hit = CircadianAnalysis._calculate_srm_hit(series,
-                                                           lower_limit=lower,
-                                                           upper_limit=upper)
-                l.append(hit)
-
-        return sum(l)/len(l)
-
-    @staticmethod
-    def calculate_srm_across_users(df,
-                                   user_col='user_id',
-                                   **srm_args):
-        """
-        Calculates SRM score across users.
-        """
-
-        l = []
-        for k, v in df.groupby(user_col):
-            srm = CircadianAnalysis.calculate_srm(v, **srm_args)
-            l.append({'user_id': k, 'srm': srm})
-
-        return pd.DataFrame(l)
-
-    @staticmethod
-    def rolling_srm_across_users(df, start_date,
-                                 how_many_days,
-                                 time_col='completion_time',
-                                 **srm_args):
-        """
-        Calculates rolling SRM across days.
-        """
-        srm_df = None
-        for i in range(how_many_days):
-            s = start_date + datetime.timedelta(days=i)
-            e = s + datetime.timedelta(days=7)
-
-            w = df[df[time_col].map(lambda z: z >= s and z < e)]
-            df_w = CircadianAnalysis.calculate_srm_across_users(w,
-                                                                time_col=time_col,
-                                                                **srm_args)
-            df_w['date'] = s.date()
-            srm_df = pd.concat([srm_df, df_w], ignore_index=True)
-
-        return srm_df
+    return series
 
 
-class CosineCurveFitting(object):
+def _calculate_srm_hit(series,
+                       lower_limit,
+                       upper_limit):
     """
-    Performs Cosine curve fitting.
+    Calculates number of hits.
 
-    Harmonic cosine analysis is often used to model Circadian
-    phenomenon. In particular, Vetter et al. [1] uses the following
-    equation to model alertness:
+    Hit is defined as the number of values z falling within
+    the given range: lower_limit >= z <= upper_limit.
 
-    f(t) = a * sin(t/12 * π) + b * cos(t/12 * π) + C
+    Parameters
+    ----------
+    series : Series
+       Decimal values indicating the time of event (after
+       converting using `_convert_timestamp_to_decimal`).
+    lower_limit : float
+        Lower limit
 
-    This class performs cosine curve fitting along with error reporting
-    and plotting.
-
+    Returns
+    -------
+    int
+        Number of hits.
     """
 
-    def __init__(self, df, y_col):
-        """
-        Initiates the class.
+    filtering_f = lambda z: z >= lower_limit and z <= upper_limit
 
-        Parameters
-        ----------
+    return sum(series.map(filtering_f))
 
-        df: DataFrame with `DateTimeIndex`.
 
-        y_col: Column name that would be used for fitting value (output).
-        """
+def calculate_srm(df, target_col,
+                  time_col='completion_time',
+                  min_samples=3):
+    """
+    Calculates SRM score.
 
-        raise NotImplemented
+    Parameters
+    ----------
 
-    @staticmethod
-    def _get_formatted_df(df, y_col, bin_width_min=30):
-        """
-        Formats data for subsequent use.
+    df : DataFrame.
+    target_col : str
+        Column with target names. Individual hits would be
+        calculated for each targets.
+    time_col : str
+        Column containing timestamps. Default is 'completion_time'.
+    min_samples : int
+        Minimum samples for calculating hit
+        for a given column. Default is 3 (40%
+        of a week).
 
-        This function ensures that X-Axis values are in hour scale.
-        """
+    Returns
+    -------
+    float
+        Value within [0, 7] range indicating overall SRM stability.
+    """
 
-        raise NotImplemented
+    hit_range = 45/60  # "hit" if falls within 45 minute
 
-    def perform_fit(self, p0=None):
-        return scipy.optimize.curve_fit(CosineCurveFitting._fit_function,
-                                        xdata=self.X, ydata=self.Y, p0=p0)
+    l = []
 
-    @staticmethod
-    def _fit_function(t, a, b):
-        """
-        Fit function to be used passed to curve_fit.
+    for k, v in df.groupby(target_col):
+        series = v.loc[:, time_col]
+        series = _srm_preprocssing(series)
+        if len(series) >= min_samples:
 
-        """
+            mean = series.mean()
 
-        arg = t/12 * np.pi
-        return a * np.sin(arg) + b * np.cos(arg)
+            lower = mean - hit_range
+            upper = mean + hit_range
+
+            hit = _calculate_srm_hit(series, lower_limit=lower,
+                                     upper_limit=upper)
+            l.append(hit)
+
+    return sum(l)/len(l)
+
+
+def _calculate_srm_across_users(df,
+                                user_col='user_id',
+                                **srm_args):
+    """
+    Calculates SRM score across users.
+
+    Parameters
+    ----------
+    df : DataFrame
+    user_col : str
+        User id column. Default is 'user_id'.
+    **srm_args
+        Variable args. See `calculate_srm` for options.
+
+    Returns
+    -------
+    DataFrame
+        A DataFrame with user_id and srm columns.
+    """
+
+    l = []
+    for k, v in df.groupby(user_col):
+        srm = calculate_srm(v, **srm_args)
+        l.append({'user_id': k, 'srm': srm})
+
+    return pd.DataFrame(l)
+
+
+def rolling_srm_across_users(df, start_date,
+                             how_many_days,
+                             time_col='completion_time',
+                             **srm_args):
+    """
+    Calculates rolling SRM across days for given days.
+
+    Parameters
+    ----------
+    df : DataFrame
+    start_date : DateTime
+    how_many_days : int
+        Number of days for which the rolling SRM
+        should be calculated. In other words, SRM
+        would be calculated for each week starting
+        at d where start_date <= d <= start_date + how_many_days.
+    time_col : str
+        Column indicating completion time. Default is 'completion_time'.
+    **srm_args
+        Variable args. For options, see `calculate_srm`.
+
+    Returns
+    -------
+    DataFrame
+        A DataFrame with user_id, srm, date columns. The date column
+        indicate the first day of each week on which SRM has
+        been calculated.
+    """
+    srm_df = None
+    for i in range(how_many_days):
+        s = start_date + dt.timedelta(days=i)
+        e = s + dt.timedelta(days=7)
+
+        w = df[df[time_col].map(lambda z: z >= s and z < e)]
+        df_w = _calculate_srm_across_users(w, time_col=time_col, **srm_args)
+
+        df_w['date'] = s.date()
+        srm_df = pd.concat([srm_df, df_w], ignore_index=True)
+
+    return srm_df
