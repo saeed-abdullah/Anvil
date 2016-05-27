982c618df89a50283d58f356328d5037c26f0cc2
Saeed Abdullah <sma249@cornell.edu>
2016-05-27 18:43:03 -0400

    Exposes more functions through api

--- anvil/api.py
+++ anvil/api.py
@@ -8,6 +8,7 @@
     :copyright: (c) 2016 by Saeed Abdullah.
 """
 
-from .utils import convert_time_zone
+from .utils import convert_time_zone, get_hourly_distribution
 from .circadian import inter_daily_stability, intra_daily_variability,\
     calculate_srm, rolling_srm_across_users
+from .location import do_location_clustering, daily_location_cluster_count
